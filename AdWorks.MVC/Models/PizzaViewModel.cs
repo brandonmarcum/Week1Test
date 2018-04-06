21f1aa72ecb8071949f98ce79a092df64012f150
Brandon Marcum <brandon.marcum9@gmail.com>
2018-04-06 11:30:51 -0400

    ADWORKZ HIJACK!!!

---
+++ AdWorks.MVC/Models/PizzaViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AdWorks.MVC.Models
+{
+  public class PizzaViewModel
+  {
+    [Required(Message = "the message")]
+    [Datatype(Datatype.Email)]
+    [StringLength]
+    [NameValidator(Message = "")]
+    public string Name
+    {
+      get;
+      set;
+    }
+
+    public string Size
+    {
+      get;
+      set;
+    }
+
+    public List<string> ToppingList
+    {
+      get;
+      set;
+    }
+
+    public Size Sizes
+    {
+      get;
+      set;
+    }
+
+    public Topping Toppings
+    {
+      get;
+      set;
+    }
+
+    public PizzaViewModel()
+    {
+      Name = "fred";
+      Sizes = new Size();
+      Toppings = new Topping();
+    }
+  }
+}
